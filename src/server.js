de8a52a5efe859da878634854a65896debd395b4
NandhuTee <rtnandhutee@gmail.com>
2026-03-02 10:47:51 +0530

    Fix port binding for Render deployment

--- src/server.js
+++ src/server.js
@@ -28,6 +28,9 @@ app.get("/", (req, res) => {
 app.use("/experience", experienceRoutes);
 app.use("/links", socialRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
